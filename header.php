5c480405b5f0fe9146fc7f9c07dafd6852974a24
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-07-18 22:16:20 +0700

    Initial commit

---
+++ header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Phu Hanh Machinery & Engineering Co., Ltd.</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="style-cty.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="card-header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light align-items-center flex-lg-column fixed-top">
+            <a class="navbar-brand justify-content-md-start" href="#"><img class="" src="images/logo.png"
+                    class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
+                    alt="logo-header" /></a>
+            <button class="navbar-toggler d-lg-none align-self-center m-0" type="button" data-toggle="collapse"
+                data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavId">
+                <ul class="navbar-nav mr-0 mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">engineering</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Factory Building</a>
+                            <a class="dropdown-item" href="#">Construction project</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Air Compressor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Industrial Brush</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Cutting tools & Machinery
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Cutting tools</a>
+                            <a class="dropdown-item" href="#">Machinery</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">about us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
---
+++ index.php
@@ -0,0 +1,140 @@
+
+    <div class="card-body">
+        <div id="maincarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+            <ol class="carousel-indicators">
+                <li data-target="#maincarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#maincarousel" data-slide-to="1"></li>
+                <li data-target="#maincarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/wire-brushes-banner.jpg" class="d-block w-100" alt="..." />
+                    <div class="carousel-caption d-block">
+                        <h5>Industrial Brush</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/Cuttingtool.jpg" class="d-block w-100" alt="..." />
+                    <div class="carousel-caption d-block">
+                        <h5>Cutting tools</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/resolve.jpg" class="d-block w-100" alt="..." />
+                    <div class="carousel-caption d-block">
+                        <h5>Collaboration</h5>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#maincarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#maincarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <div class="box row p-md-5 p-4">
+            <div class="col-12 col-md-6 col-lg-4 py-2 rounded-2">
+                <div class="card text-dark h-100 p-1">
+                    <img src="images/Box/AirCompresser.jpg" class="card-img hover-shadow" alt="..." />
+                    <div class="card-body m-0">
+                        <h5 class="card-title">Air Compressor</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum officiis voluptate quo
+                            corporis porro, cumque repellat consequuntur odit soluta, laboriosam nobis voluptatum
+                            voluptatem sunt in sequi error amet quam ipsum.
+                        </p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 py-2 rounded-2">
+                <div class="card text-dark h-100 p-1">
+                    <img src="images/Box/brush.jpg" class="card-img" alt="..." />
+                    <div class="card-body m-0">
+                        <h5 class="card-title">Industrial Brush</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 py-2 rounded-2">
+                <div class="card text-dark h-100 p-1">
+                    <img src="images/Box/cuttingtool.jpg" class="card-img" alt="..." />
+                    <div class="card-body m-0">
+                        <h5 class="card-title">Cutting Tools</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 py-2 rounded-2">
+                <div class="card text-dark h-100 p-1">
+                    <img src="images/Box/d1111ed6.png" class="card-img" alt="..." />
+                    <div class="card-body m-0">
+                        <h5 class="card-title">Trading</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 py-2 rounded-2">
+                <div class="card text-dark h-100 p-1">
+                    <img src="images/Box/engineering.jpg" class="card-img" alt="..." />
+                    <div class="card-body m-0">
+                        <h5 class="card-title">Engineering</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 py-2 rounded-2">
+                <div class="card text-dark h-100 p-1">
+                    <img src="images/Box/service.jpg" class="card-img" alt="..." />
+                    <div class="card-body m-0">
+                        <h5 class="card-title">Service</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                    <i class="fas fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+        </div>
+
+    </div>
